a4a8df1ed7531fbf45f02dfdd0428215b08d55dc
Igor Vieira <higorc2@gmail.com>
2016-11-07 20:11:20 -0200

    Create Users model and Tasks model

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 FROM node:4.3.0
 
 RUN useradd --user-group --create-home --shell /bin/false app &&\
-  npm install --global npm@3.7.5
+  npm install --global --silent npm@3.7.5
 
 ENV HOME=/home/app
 
--- libs/boot.js
+++ libs/boot.js
@@ -1,7 +1,7 @@
 // Module responsable for the server initialization.
 
 module.exports = app => {
-  app.db.sync().done(() => {
+  app.db.sequelize.sync().done(() => {
     app.listen(app.get('port'), () => {
       console.log(`Server listenning at port ${app.get('port')}`);
     });
