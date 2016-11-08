7dfd7a4fb44e541a73479e3d39427528a0f7d15d
Igor Vieira <higorc2@gmail.com>
2016-11-08 13:07:10 -0200

    Enable CORS in the API

--- libs/middlewares.js
+++ libs/middlewares.js
@@ -1,7 +1,13 @@
 import bodyParser from 'body-parser';
+import cors from 'cors';
 
 module.exports = app => {
   app.set('port', 3000);
+  app.use(cors({
+    origin: ['http://localhost:3001'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+  }));
   app.use(bodyParser.json());
   app.use(app.auth.initialize());
   app.use((req, res, next) => {
