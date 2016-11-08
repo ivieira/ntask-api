7e9948eba38a6cce4a9048e70a98438aee9c7368
Igor Vieira <higorc2@gmail.com>
2016-11-08 15:53:41 -0200

    Apply SSL support to use HTTPS

--- libs/boot.js
+++ libs/boot.js
@@ -1,8 +1,15 @@
 // Module responsable for the server initialization.
+import https from 'https';
+import fs from 'fs';
+
 module.exports = app => {
   if (process.env.NODE_ENV !== 'test') {
+    const credentials = {
+      key: fs.readFileSync('ntask.key', 'utf8'),
+      cert: fs.readFileSync('ntask.cert', 'utf8')
+    }
     app.db.sequelize.sync().done(() => {
-      app.listen(app.get('port'), () => {
+      https.createServer(credentials, app).listen(app.get('port'), () => {
         console.log(`Server listenning at port ${app.get('port')}`);
       });
     });
